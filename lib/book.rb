461d31d66073c24df41e5a3d40ccca24b39bc207
bcaudillo <btcaudillo@gmail.com>
2022-12-07 15:03:00 -0700

    commit tree 41d2cde1c4f966388040b0c5c348cb508d957621

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,18 @@
-
-
+class Book
+    attr_accessor :title, :author, :page_count, :genre, :turn_page
+    def initialize(title)
+        @title = title
+    end
+    def author=(name)
+        @author = name
+    end
+    def page_count=(count)
+        @page_count = count
+    end
+    def genre=(name)
+        @genre = name
+    end 
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
